acbfa67e5d1903985aa236d1da7ca493285f131f
nosch <norbert.schmidt@mayflower.de>
2014-02-12 22:12:40 +0100

    New structure and content for 'Settings'.

--- app/js/module/settings/settings-config.js
+++ app/js/module/settings/settings-config.js
@@ -12,6 +12,40 @@ angular.module('settings.config', [
         $stateProvider
             .state('settings', {
                 url: '/settings',
-                templateUrl: 'js/module/settings/view/settings.tpl.html'
+                templateUrl: 'js/module/settings/view/settings.tpl.html',
+                abstract: true
+            })
+            .state('settings.first', {
+                url: '/first',
+                views: {
+                    'one': {
+                        templateUrl: 'js/module/settings/view/first/one.tpl.html'
+                    },
+                    'two': {
+                        templateUrl: 'js/module/settings/view/first/two.tpl.html'
+                    }
+                }
+            })
+            .state('settings.second', {
+                url: '/second',
+                views: {
+                    'one': {
+                        templateUrl: 'js/module/settings/view/second/one.tpl.html'
+                    },
+                    'two': {
+                        templateUrl: 'js/module/settings/view/second/two.tpl.html'
+                    }
+                }
+            })
+            .state('settings.second.more', {
+                url: '/more',
+                views: {
+                    'a': {
+                        templateUrl: 'js/module/settings/view/second/more/a.tpl.html'
+                    },
+                    'b': {
+                        templateUrl: 'js/module/settings/view/second/more/b.tpl.html'
+                    }
+                }
             });
     });
